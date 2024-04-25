fe5ef298516c935b59186ad48aee13fc0800fc36
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-04-26 00:42:14 +0600

    Js Array

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+let number1 = 10;
+let number2 = 20;
+
+let result = 10+20;
+
+console.log("The result is="+result); // The result is = 30
+
+let firstName = "Abdul";
+let lastName  = 'Jobbar';
+
+console.log(firstName+' '+lastName); //Abdul+Jobbar //Rahim+10 //10+50
+
+// Array....
+
+let marks = [97, 97, 90, 95, 89, 79, 80, 85, 80, 85];
+
+console.log(marks[0]);
+console.log('The value in position 1='+marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+
+let human = {firstName:'Abdul', lastName:'Jobbar', age:12, balance:40};
+
+console.log('First Name is='+human.firstName);
+console.log('Last Name is='+human.lastName);
+console.log('Age is='+human.age);
+console.log('Balance is='+human.balance);
+
+
+let twoDimensional =[
+    [10, 3, 4, 5],
+    [15, 4, 6, 10],
+    [15, 4, 7, 12],
+    [15, 4, 7, 12],
+    [15, 4, 7, 12],
+]
+
+console.log(twoDimensional[1][3]);
+console.log(twoDimensional[3][0]);
+console.log(twoDimensional[3][0]);
